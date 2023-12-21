7e5bc36b9f506562d723d7fed571db93dc37836e
Charly <66905063+EnisuVI@users.noreply.github.com>
2023-12-21 08:27:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from math import*
 csteIncid=1
 cstRefl=1
 temps={1/1000, 1/500, 1/250, 1/125, 1/60, 1/30, 1/15, 1/8, 1/4, 1/2, 1}
-ouverture={16, 11, 8, 5.6, 4, 2.8, 2}
+ouverture={16, 11, 8, 5.6, 4, 2.8, 2, 1.4}
 def main():
     lux=int(input("Quantité de lumière en Lux ? (Mesure lumière incidente) "))
     luminance=int(input("\nValeur de la luminance ? (Mesure lumière réfléchie) "))
